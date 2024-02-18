0f02943a78a6e5d3b25b34ecba765cf8b006d378
mmedchuk <m.medchuk@gmail.com>
2024-02-18 23:46:03 +0200

    Models added and settings updates are implemented

--- app/core.py
+++ app/core.py
@@ -1,6 +1,6 @@
 from sqlalchemy import text, insert
 from database import sync_engine, sync_session_factory, Base
-from models import WorkerOrm
+from models import WorkerOrm, ResumeOrm
 
 
 
@@ -16,10 +16,19 @@ def insert_data():
     with sync_session_factory() as conn:
         worker_1 = WorkerOrm(name='Olha')
         worker_2 = WorkerOrm(name='sv')
-        conn.add(worker_1)
-        conn.add(worker_2)
+        resume_1 = ResumeOrm(title='Senior Python', salary=1000, workload='parttime', worker_id=2)
+        resume_2 = ResumeOrm(title='Junior Python', salary=3000, workload='fulltime', worker_id=1)
+        conn.add_all([worker_1, worker_2])
+        conn.commit()
+        conn.add_all([resume_1, resume_2])
         conn.commit()
         
+insert_data()
+
+
+
+
+
 
 # def insert_data_ns():
 #     with sync_engine.connect() as conn:
@@ -30,5 +39,3 @@ def insert_data():
 #         conn.execute(stmt)
 
 #         conn.commit()
-
-insert_data()
--- app/models.py
+++ app/models.py
@@ -1,27 +1,56 @@
+from datetime import datetime
+from enum import Enum
+from typing import Annotated
 from sqlalchemy.orm import Mapped, mapped_column
 from database import Base
-from sqlalchemy import Table, Column, String, MetaData, Integer
+from sqlalchemy import ForeignKey, Table, Column, String, MetaData, Integer, text
 
 
-metadata_obj= MetaData()
-
-
-workers_table = Table(
-    'workers',
-    metadata_obj,
-    Column('id', Integer, primary_key=True),
-    Column('username', String)
-)
-
-
-
-
-
+intpk = Annotated[int, mapped_column(primary_key=True)]
+created_at = Annotated[datetime, mapped_column(server_default=text("TIMEZONE('utc', now())"))]
+updated_at = Annotated[datetime, mapped_column(server_default=text("TIMEZONE('utc', now())"),
+        onupdate=datetime.utcnow())]
 
 
 class WorkerOrm(Base):
     __tablename__ = 'workers'
 
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[intpk]
     name:Mapped[str]
+
+
+class Workload(Enum):
+    parttime = "parttime"
+    fulltime = "fulltime"
+
+
+class ResumeOrm(Base):
+    __tablename__ = 'resumes'
+
+    id: Mapped[intpk]
+    title : Mapped[str]
+    salary: Mapped[int | None]
+    workload: Mapped[Workload]
+    worker_id: Mapped[int] = mapped_column(ForeignKey("workers.id", ondelete="CASCADE"))
+    created_at: Mapped[created_at]
+    updated_at: Mapped[updated_at]
+
+
+
+
+
+
+
+
+# metadata_obj= MetaData()
+
+
+# workers_table = Table(
+#     'workers',
+#     metadata_obj,
+#     Column('id', Integer, primary_key=True),
+#     Column('username', String)
+# )
+
+
 
